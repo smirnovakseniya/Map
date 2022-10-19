6318fe9bd30b4ba56a4f0c9300a80cb064e87cb9
Ксения Смирнова <smirnova.kseniya.xs@gmail.com>
2022-10-19 13:44:24 +0300

    Create

---
+++ Map/Model/MapModel.swift
@@ -0,0 +1,84 @@
+//
+//  MapModel.swift
+//  Map
+//
+//  Created by Kseniya Smirnova on 19.10.22.
+//
+
+import UIKit
+import MapKit
+
+class MapModel {
+    
+    weak var timer: Timer?
+    
+    var flightpathPolylineLast: MKGeodesicPolyline!
+    var flightpathPolylineNext: MKGeodesicPolyline!
+    var planeAnnotation: MKPointAnnotation!
+    var planeAnnotationPosition = 0
+    let annotation = MKPointAnnotation()
+    var isHiddenPath = false
+     
+    let point1 = CLLocationCoordinate2D(latitude: 53.893009, longitude: 27.567444)
+    let point2 = CLLocationCoordinate2D(latitude: 52.097622, longitude: 23.734051)
+    //        let point3 = CLLocationCoordinate2D(latitude: 54.687157, longitude: 25.279652)
+    
+    func addPolyline(mapView: MKMapView) {
+        flightpathPolylineLast = createPolyline(mapView: mapView, point1: point1, point2: point2)
+        planeAnnotation = annotation
+        
+        updatePlanePositionLast(mapView: mapView)
+    }
+    
+    func updatePlanePositionLast(mapView: MKMapView) {
+        let step = 1
+        if planeAnnotationPosition + step < flightpathPolylineLast.pointCount {
+            let points = flightpathPolylineLast.points()
+            self.planeAnnotationPosition += step
+            let nextMapPoint = points[planeAnnotationPosition]
+            planeAnnotation.coordinate = nextMapPoint.coordinate
+            flightpathPolylineNext = createPolyline(mapView: mapView, point1: point1, point2: planeAnnotation.coordinate)
+            timer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: false) { _ in
+                self.updatePlanePositionLast(mapView: mapView)
+            } //  perform(#selector(updatePlanePositionLast), with: nil, afterDelay: 0.03)
+        } else {
+            timer?.invalidate()
+        }
+    }
+    
+    func createPolyline(mapView: MKMapView, point1: CLLocationCoordinate2D, point2: CLLocationCoordinate2D) -> MKGeodesicPolyline {
+        let locations = [point1, point2]
+        let geodesicPolyline = MKGeodesicPolyline(coordinates: locations, count: locations.count)
+        mapView.addOverlay(geodesicPolyline)
+        return geodesicPolyline
+    }
+    
+    //MARK: - Degrees
+    
+    func degreesToRadians(_ degrees: Double) -> Double {
+        return degrees * .pi / 180.0
+    }
+    func radiansToDegrees(_ radians: Double) -> Double {
+        return radians * 180.0 / .pi - .pi
+    }
+    func getBearingBetweenTwoPoints(point1: CLLocationCoordinate2D, point2: CLLocationCoordinate2D) -> Double {
+        let lat1 = degreesToRadians(point1.latitude)
+        let lon1 = degreesToRadians(point1.longitude)
+
+        let lat2 = degreesToRadians(point2.latitude)
+        let lon2 = degreesToRadians(point2.longitude)
+
+        let dLon = lon2 - lon1
+
+        let y = sin(dLon) * cos(lat2)
+        let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon)
+        let radiansBearing = atan2(y, x)
+
+        return radiansToDegrees(radiansBearing)
+    }
+    
+    func addPoints() -> Double {
+        return getBearingBetweenTwoPoints(point1: point1, point2: point2)
+    }
+    
+}
---
+++ Map/VC/MapVC.swift
@@ -0,0 +1,95 @@
+//
+//  MapVC.swift
+//  Map
+//
+//  Created by Kseniya Smirnova on 17.10.22.
+//
+
+import UIKit
+import MapKit
+
+class MapVC: UIViewController, UIViewControllerTransitioningDelegate, FirstViewControllerDelegate {
+    func update(text: Bool) {
+        model.isHiddenPath = text
+    }
+    
+    private let model = MapModel()
+    private let infoVC = InfoVC()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.delegate = self
+        
+        model.addPolyline(mapView: mapView)
+        mapView.addAnnotation(model.annotation)
+        
+        infoVC.delegate = self
+    }
+}
+
+extension MapVC: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let planeIdentifier = "Plane"
+        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: planeIdentifier)
+        ?? MKAnnotationView(annotation: annotation, reuseIdentifier: planeIdentifier)
+        annotationView.image = UIImage(systemName: "airplane")
+        annotationView.transform = CGAffineTransformRotate(mapView.transform, model.addPoints())
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        model.isHiddenPath.toggle()
+        
+        if #available(iOS 15.0, *) {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: "infoVC")
+            if let presentationController = viewController.presentationController as? UISheetPresentationController {
+                presentationController.detents = [.medium(), .large()]
+            }
+            self.present(viewController, animated: true, completion: nil)
+        } else {
+//            let yourVC = ViewController()
+//            yourVC.modalPresentationStyle = .custom
+//            yourVC.transitioningDelegate = self
+//            self.present(yourVC, animated: true)
+        }
+        /// аннотация, на которую было нажата, держалась выбранной. код ниже отменяет выбор аннотации, и после этого можно нажать во второй раз.
+        DispatchQueue.main.async {
+            for item in self.mapView.selectedAnnotations {
+                self.mapView.deselectAnnotation(item, animated: false)
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard let polyline = overlay as? MKPolyline else { return MKOverlayRenderer() }
+        let renderer = MKPolylineRenderer(polyline: polyline)
+        
+        if model.isHiddenPath {
+            if polyline == model.flightpathPolylineLast {
+                renderer.lineWidth = 2.0
+                renderer.strokeColor = .red
+            } else {
+                renderer.lineWidth = 2.0
+                renderer.strokeColor = .blue
+            }
+        } else {
+            mapView.removeOverlays(mapView.overlays)
+        }
+        return renderer
+    }
+}
+
+class MyAnnotation: NSObject,MKAnnotation {
+
+var info : String
+var coordinate : CLLocationCoordinate2D
+
+init(coordinate: CLLocationCoordinate2D, info: String) {
+    self.coordinate = coordinate
+    self.info = info
+}
+}
